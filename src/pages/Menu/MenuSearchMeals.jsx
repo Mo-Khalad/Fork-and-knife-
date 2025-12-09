dcdba039b164a5b458506a1db9f02f296c2b3136
mohmed <mohmedkhalad390@gmail.com>
2025-12-09 23:32:39 +0200

    design ui ux part one

--- src/pages/Menu/MenuSearchMeals.jsx
+++
@@ -1,8 +0,0 @@
-import React from "react";
-export const MenuSearchMeals = ({mealName}) => {
-  return (
-        <li className="m-3 cursor-pointer hover:bg-hover-color transition duration-700">
-          { mealName }
-        </li>
-  );
-};
